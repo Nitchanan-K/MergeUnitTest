a369ab819367d62fd26fe7083a1576292a8d6e52
nitchanan.k <nitchanan_katinasatian@hotmail.com>
2023-07-04 15:58:41 +0700

    first commit

---
+++ test/merge.test.ts
@@ -0,0 +1,10 @@
+import { merge } from '../src/merge';
+
+describe("Merge function", () => {
+    it("should return a merged array of sorted array", () => {
+        const expectedResult = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        const result = merge([1, 3 ,5, 7, 9],[2, 4, 6, 8, 10]);
+        expect(result).toEqual(expectedResult);
+    });
+})
+
